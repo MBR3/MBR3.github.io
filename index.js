863362b9c47c72d3251f3f1b6a9f0752ec425d9b
MBR3 <66896079+MBR3@users.noreply.github.com>
2020-09-19 08:55:24 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-window.onload = function () {
-    var divObj = document.getElementById("huzake");
-    var brNum = 20;
-    for (var i = 0; i < imageNum; i++) {
-        var text = document.createTextNode("今晩は。明日も頑張りましょう。");
-        divObj.appendChild(text);
+// JavaScript source code
+window.CookieSaveOmoshiroi = function CookieSaveOmoshiroi() {
+    this.alert("ありがとう");
+    document.cookie = "omoshiroi=" + encodeURIComponent('へった');
+}
+
+window.onload = function cookieKakunin() {
+    if (document.cookie == '') {
+        alert('no cookie');
     }
-    var text = document.createTextNode("今晩は。明日も頑張りましょう。");
-    divObj.appendChild(text);
+    else {
+        alert('yes cookie');
+    }
 }

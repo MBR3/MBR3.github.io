af049015e7a93fa4b5fafe8ca3f13ed94445a424
MBR3 <66896079+MBR3@users.noreply.github.com>
2020-09-19 08:20:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var divObj = document.getElementById("huzake");
+    var brNum = 20;
+    for (var i = 0; i < imageNum; i++) {
+        var text = document.createTextNode("今晩は。明日も頑張りましょう。");
+        divObj.appendChild(text);
+    }
+    var text = document.createTextNode("今晩は。明日も頑張りましょう。");
+    divObj.appendChild(text);
+}

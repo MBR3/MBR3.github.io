98d2b1af8055336e20fda4749835d41875946f10
MBR3 <66896079+MBR3@users.noreply.github.com>
2020-09-19 10:12:07 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 // JavaScript source code
-var count = 0;
+var count;
 window.CookieSaveOmoshiroi = function CookieSaveOmoshiroi() {
-    count++;
+    count;
     this.alert("Thank you");
     document.cookie = 'omoshiroi=' + count;
 }
 
 window.onload = function cookieKakunin() {
+    var cookieValue = "";
     if (document.cookie == '') {
         alert('no cookie');
+        count = 0;
     }
     else {
         alert('yes cookie');
-        this.alert(document.cookie)
         this.alert("ÅJÉîÉBÅJÉGÅJ");
+        var cookies = document.cookie;
+        var cookieItem = cookies.split(";");        
+        for (i = 0; i < cookieItem.length; i++) {
+            var elem = cookieItem[i].split("=");
+            if (elem[0].trim() == "omoshiroi") {
+                cookieValue = elem[1];
+            } else {
+                continue;
+            }
+        }
+        this.alert(cookieValue);
+        count = cookieValue;
     }
 }
